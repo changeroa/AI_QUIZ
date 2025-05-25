c0bd9627a7c1d7a35fd60f20d6469b6024d4158d
Honghyu Koh <issackoh@naver.com>
2025-05-25 21:41:38 +0900

    0525 트랜스포머모델 추가가

--- PDF_preprocessing/rule-based 오타교정.py
+++ PDF_preprocessing/rule-based 오타교정.py
@@ -1,12 +1,13 @@
 from transformers import PreTrainedTokenizerFast, BartForConditionalGeneration
 
-
 tokenizer = PreTrainedTokenizerFast.from_pretrained("hyunwoongko/kobart")
 model = BartForConditionalGeneration.from_pretrained("hyunwoongko/kobart")
 
 input_text = "나는 어제 도서관에 가따."
 input_ids = tokenizer.encode(input_text, return_tensors="pt")
-output_ids = model.generate(input_ids, max_length=64)
+output_ids = model.generate(input_ids, max_length=64, num_beams=4, early_stopping=True)
 output = tokenizer.decode(output_ids[0], skip_special_tokens=True)
 
-print(output)  # → 나는 어제 도서관에 갔다.
+print("=== 출력 시작 ===")
+print(output)
+print("=== 출력 끝 ===")
